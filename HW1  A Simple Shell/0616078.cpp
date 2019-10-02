5398eaa8c11d34f824889088d740a39481e80ca0
tommytyc <tommytyc.cs06@nctu.edu.tw>
2019-10-02 23:38:25 +0800

    fix the pointer problem. haven't supported pipe function.

--- HW1  A Simple Shell/0616078.cpp
+++ HW1  A Simple Shell/0616078.cpp
@@ -12,10 +12,12 @@ Statement: I am fully aware that this program is not supposed to be posted to a 
 #include <stdio.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <vector>
 using namespace std;
 
 int main(){
 	pid_t pid;
+	int exv = 0;// the return value of execvp
 	bool cnt = false;
 	char* p;
 	pid = fork();
@@ -25,23 +27,35 @@ int main(){
 		exit(-1);
 	}
 	else if(pid == 0){//in child process
-			char* cstr = new char[256];
-			cout<<">";
-			cin.getline(cstr, 256);
-			p = strtok(cstr, " ");
-			while(1){
-				if(cnt){
-					execlp(cstr, cstr, p, NULL);
-				}
-				else{
-					p = strtok(NULL, "");
-					cnt = true;
-				}
-			}
+		char* cstr1 = new char[256];
+		vector<string> vstr;
+		cout<<">";
+		cin.getline(cstr1, 256);
+		p = strtok(cstr1, " ");
+		while(p != NULL){
+			vstr.push_back(p);
+			p = strtok(NULL, " ");
+		}
+		delete [] cstr1;
+
+		//dynamic memory allocation
+		char** cstr2 = new char*[vstr.size()+1];
+		for(int i = 0; i <= vstr.size(); i++){
+			cstr2[i] = new char[256];
+		}
+
+		//copy the string
+		for(int i = 0; i < vstr.size(); i++){
+			strcpy(cstr2[i], vstr[i].c_str());
+		}
+		cstr2[vstr.size()] = 0;
+		exv = execvp(cstr2[0], cstr2);
+		for(int i = 0; i < vstr.size(); i++){
+			cout<<*cstr2[i]<<endl;
+		}
 	}
 	else{//in parent process
 		wait(NULL);
-		// exit(0);
 	}
 	return 0;
 }
