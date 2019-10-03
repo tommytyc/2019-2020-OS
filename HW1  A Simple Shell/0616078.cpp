9e04c0d614333bb718c47e08f6e0b0cbe2d0ee87
tommytyc <tommytyc.cs06@nctu.edu.tw>
2019-10-04 04:17:38 +0800

    finish the task without using system().

--- HW1  A Simple Shell/0616078.cpp
+++ HW1  A Simple Shell/0616078.cpp
@@ -7,19 +7,14 @@ Statement: I am fully aware that this program is not supposed to be posted to a 
 */
 #include <iostream>
 #include <unistd.h>
-#include <string>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
 #include <string.h>
-#include <vector>
 using namespace std;
 
 int main(){
 	pid_t pid;
-	int exv = 0;// the return value of execvp
-	bool cnt = false;
-	char* p;
 	pid = fork();
 	//fork another process
 	if(pid < 0){//for error situation
@@ -28,19 +23,27 @@ int main(){
 	}
 	else if(pid == 0){//in child process
 		while(1){
-			char* cstr1 = new char[256];
-			cout<<">";
-			cin.getline(cstr1, 256);
-			int status;
-			status = system(cstr1);
-			if(status == -1){
-				cerr<<"Error.\n";
+			pid_t tmppid;
+			tmppid = fork();
+			if(tmppid < 0){
+				cerr<<"Child Process Fork Failed!\n";
+			}
+			else if(tmppid == 0){
+				char* cstr1 = new char[256];
+				cout<<">";
+				cin.getline(cstr1, 256);
+				execl("/bin/sh", "sh", "-c", cstr1, NULL);
+				cout<<"successfully executed!\n";
+			}
+			else{
+				wait(NULL);
 			}
 		}
-		exit(0);		
+		exit(0);
 	}
 	else{//in parent process
 		wait(NULL);
+		exit(0);
 	}
 	return 0;
 }
