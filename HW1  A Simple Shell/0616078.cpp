c5c0af02cf1e0b7aa482a28c4ffba136564c52b3
tommytyc <tommytyc.cs06@nctu.edu.tw>
2019-10-04 14:04:02 +0800

    found that we can't use sh.

--- HW1  A Simple Shell/0616078.cpp
+++ HW1  A Simple Shell/0616078.cpp
@@ -1,20 +1,25 @@
 /*
 Student No.: 0616078
 Student Name: 唐宇謙
-Email: tommytyc.cs06@nctu.edu.tw
+Email: .tommytyc.cs06@nctu.edu.tw
 SE tag: xnxcxtxuxoxsx
 Statement: I am fully aware that this program is not supposed to be posted to a public server, such as a public GitHub repository or a public web page.
 */
 #include <iostream>
 #include <unistd.h>
+#include <string>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <vector>
 using namespace std;
 
 int main(){
 	pid_t pid;
+	int exv = 0;// the return value of execvp
+	bool cnt = false;
+	char* p;
 	pid = fork();
 	//fork another process
 	if(pid < 0){//for error situation
@@ -30,10 +35,28 @@ int main(){
 			}
 			else if(tmppid == 0){
 				char* cstr1 = new char[256];
+				vector<string> vstr;
 				cout<<">";
 				cin.getline(cstr1, 256);
-				execl("/bin/sh", "sh", "-c", cstr1, NULL);
-				cout<<"successfully executed!\n";
+				p = strtok(cstr1, " ");
+				while(p != NULL){
+					vstr.push_back(p);
+					p = strtok(NULL, " ");
+				}
+				delete [] cstr1;
+
+				//dynamic memory allocation
+				char** cstr2 = new char*[vstr.size()+1];
+				for(int i = 0; i <= vstr.size(); i++){
+					cstr2[i] = new char[256];
+				}
+
+				//copy the string
+				for(int i = 0; i < vstr.size(); i++){
+					strcpy(cstr2[i], vstr[i].c_str());
+				}
+				cstr2[vstr.size()] = 0;
+				exv = execvp(cstr2[0], cstr2);
 			}
 			else{
 				wait(NULL);
