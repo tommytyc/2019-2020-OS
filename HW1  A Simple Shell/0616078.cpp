b5e321604f0f5b11582b1b5972ed166f1ef9fc2d
tommytyc <tommytyc.cs06@nctu.edu.tw>
2019-10-07 17:32:15 +0800

    fix the prompt bug. trying to add the pipe function.

--- HW1  A Simple Shell/0616078.cpp
+++ HW1  A Simple Shell/0616078.cpp
@@ -1,7 +1,7 @@
 /*
 Student No.: 0616078
 Student Name: 唐宇謙
-Email: .tommytyc.cs06@nctu.edu.tw
+Email: tommytyc.cs06@nctu.edu.tw
 SE tag: xnxcxtxuxoxsx
 Statement: I am fully aware that this program is not supposed to be posted to a public server, such as a public GitHub repository or a public web page.
 */
@@ -15,6 +15,13 @@ Statement: I am fully aware that this program is not supposed to be posted to a 
 #include <vector>
 using namespace std;
 
+void Free(char** str, int size){
+	for(int i = 0; i < size; i++){
+		delete [] str[i];
+	}
+	delete [] str;
+}
+
 int main(){
 	pid_t pid;
 	int exv = 0, pipepos = 0, redpos = 0;// the return value of execvp
@@ -22,8 +29,13 @@ int main(){
 	char* p;
 	vector<string> vstr;
 	while(1){
-		char* cstr1 = new char[256];
+		//initialization
 		vstr.clear();
+		andflag = false; pipeflag = false; redflag = false;
+		pipepos = 0, redpos = 0;
+
+		int pipefd[2];
+		char* cstr1 = new char[256];
 		cout<<">";
 		cin.getline(cstr1, 256);
 		p = strtok(cstr1, " ");
@@ -39,7 +51,7 @@ int main(){
 		int len = vstr.size();
 
 		//dynamic memory allocation
-		char** cstr2 = new char*[len+1];
+		char** cstr2 = new char*[len + 1];
 		for(int i = 0; i <= len; i++){
 			cstr2[i] = new char[256];
 		}
@@ -47,6 +59,7 @@ int main(){
 		//copy the string
 		for(int i = 0; i < len; i++){
 			if(vstr[i] == "|"){
+				pipe(pipefd);
 				pipeflag = true;
 				pipepos = i;
 			}
@@ -61,18 +74,61 @@ int main(){
 
 		pid = fork();
 		if(!andflag){
-			if(pid < 0){
-				cerr<<"Fork Error!\n";
-			}
-			else if(pid == 0){//child process
-				exv = execvp(cstr2[0], cstr2);
-			}
-			else{
-				wait(NULL);
-				for(int i = 0; i <= len; i++){
-					delete [] cstr2[i];
+			if(pipeflag){//deal with pipe
+				//initialization
+				char **cpystr1 = new char*[pipepos];
+				char **cpystr2 = new char*[len - pipepos -1];
+				for(int i = 0; i < pipepos; i++){
+					cpystr1[i] = new char[256];
 				}
-				delete [] cstr2;
+				for(int i = 0; i < pipepos; i++){
+					strcpy(cpystr1[i], cstr2[i]);
+				}
+				for(int i = 0; i < (len - pipepos - 1); i++){
+					cpystr2[i] = new char[256];
+				}
+				for(int i = 0; i < (len - pipepos - 1); i++){
+					strcpy(cpystr2[i], cstr2[pipepos + i + 1]);
+				}
+				
+				if(pid < 0){
+					cerr<<"Fork Error!\n";
+				}
+				else if(pid == 0){//child process
+					close(pipefd[0]);
+					dup2(pipefd[1], 1);
+					close(pipefd[1]);
+					exv = execvp(cpystr2[0], cpystr2);
+				}
+				else{
+					close(pipefd[0]);
+					dup2(pipefd[1], 1);
+					close(pipefd[1]);
+					wait(NULL);
+
+					//free the allocated memory
+					// Free(cstr2, len);
+					// Free(cpystr2, (len - pipepos - 1));
+					execvp(cpystr1[0], cpystr1);
+				}
+			}
+			// else if(redflag){//deal with redirect
+
+			// }
+			else{
+				if(pid < 0){
+					cerr<<"Fork Error!\n";
+				}
+				else if(pid == 0){//child process
+					exv = execvp(cstr2[0], cstr2);
+				}
+				else{
+					wait(NULL);
+					for(int i = 0; i <= len; i++){
+						delete [] cstr2[i];
+					}
+					delete [] cstr2;
+				}
 			}
 		}
 
@@ -82,11 +138,11 @@ int main(){
 				if((pid2 = fork()) < 0){
 					cerr<<"Fork Error!\n";
 				}
-				else if(pid2 == 0){
+				else if(pid2 == 0){//grandchild process, supervised by init once the child process exit.
 					exv = execvp(cstr2[0], cstr2);
 				}
 				else{
-					exit(0);
+					exit(0);//make the grandchild process be supervised by init
 				}
 			}
 			else{
